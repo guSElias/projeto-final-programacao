a07cb88fe61db9b32151c54b6d37898c67a4c295
Gustavo Elias <gustavoeliasdesouza2310@gmail.com>
2025-12-05 17:30:49 -0300

    função transferência finalizada

--- class/Atleta/Atleta.cpp
+++ class/Atleta/Atleta.cpp
@@ -4,36 +4,31 @@
 
 using namespace std;
 
-Atleta::Atleta()
-{
+Atleta::Atleta() {
     this->nome = "Não identificado";
     this->idade = 0;
     this->posicoes = {};
 }
 
-Atleta::Atleta(string n, int i, vector<string> p)
-{
+Atleta::Atleta(string n, int i, vector<string> p) {
     this->nome = n;
     this->idade = i;
     this->posicoes = p;
 }
 
 // remove do clube atual, adiciona ao novo e altera o clube atual
-void Atleta::transferencia(Clube* novoClube)
-{
+void Atleta::transferencia(Clube *novoClube) {
+    clubes.push_back(clubeAtual);
     clubeAtual->venderAtleta(this);
     novoClube->comprarAtleta(this);
-    clubes.push_back(novoClube);
     clubeAtual = novoClube;
 };
 
-void Atleta::setClube(Clube* novoClube)
-{
+void Atleta::setClube(Clube *novoClube) {
     clubeAtual = novoClube;
 }
 
-void Atleta::exibir()
-{
+void Atleta::exibir() {
     cout << "=== Informações do Atleta ===\n";
 
     cout << "Nome: " << this->nome << "\n";
@@ -41,8 +36,7 @@ void Atleta::exibir()
 
     // Exibir posições
     cout << "Posições: ";
-    for (int i = 0; i < this->posicoes.size(); i++)
-    {
+    for (int i = 0; i < this->posicoes.size(); i++) {
         cout << posicoes[i] << " ";
     }
     cout << "\n";
@@ -51,41 +45,32 @@ void Atleta::exibir()
 
     // Histórico de clubes
     cout << "Clubes anteriores: ";
-    if (this->clubes.empty())
-    {
+    if (this->clubes.empty()) {
         cout << "Nenhum";
-    }
-    else
-    {
-        for (auto& c : this->clubes)
-        {
+    } else {
+        for (auto &c: this->clubes) {
             cout << c->getNome() << " ";
         }
     }
     cout << "\n\n";
 };
 
-void Atleta::registarGol()
-{
+void Atleta::registarGol() {
     this->gols += 1;
 }
 
-void Atleta::registarAssistencia()
-{
+void Atleta::registarAssistencia() {
     this->assistencias += 1;
 }
 
-string Atleta::getNome()
-{
+string Atleta::getNome() {
     return this->nome;
 }
 
-string Atleta::getPosicao()
-{
+string Atleta::getPosicao() {
     return this->posicoes[0];
 }
 
-void Atleta::registarJogosDisputados()
-{
+void Atleta::registarJogosDisputados() {
     this->jogosDisputados += 1;
 }
--- main.cpp
+++ main.cpp
@@ -8,35 +8,27 @@
 using namespace std;
 
 Campeonato superMundial = criarCampeonato();
-vector<Clube*> clubes = superMundial.getClubes();
+vector<Clube *> clubes = superMundial.getClubes();
 
-Clube* buscarClube(string nome)
-{
-    for (int i = 0; i < clubes.size(); i++)
-    {
-        if (clubes[i]->getNome() == nome)
-        {
+Clube *buscarClube(string nome) {
+    for (int i = 0; i < clubes.size(); i++) {
+        if (clubes[i]->getNome() == nome) {
             return clubes[i];
         }
     }
     return nullptr;
 }
 
-void adicionarCampeao()
-{
-    Clube* chelsea = buscarClube("Chelsea");
+void adicionarCampeao() {
+    Clube *chelsea = buscarClube("Chelsea");
     superMundial.setCampeao(chelsea);
 }
 
-Atleta* buscarAtleta(string nome)
-{
-    for (int i = 0; i < clubes.size(); i++)
-    {
-        vector<Atleta*> atletas = clubes[i]->getElenco();
-        for (int j = 0; j < atletas.size(); j++)
-        {
-            if (atletas[j]->getNome() == nome)
-            {
+Atleta *buscarAtleta(string nome) {
+    for (int i = 0; i < clubes.size(); i++) {
+        vector<Atleta *> atletas = clubes[i]->getElenco();
+        for (int j = 0; j < atletas.size(); j++) {
+            if (atletas[j]->getNome() == nome) {
                 return atletas[j];
             }
         }
@@ -44,13 +36,9 @@ Atleta* buscarAtleta(string nome)
     return nullptr;
 }
 
-
-void menu(int opcao)
-{
-    switch (opcao)
-    {
-    case 1:
-        {
+void menu(int opcao) {
+    switch (opcao) {
+        case 1: {
             string nome;
             int anoFundacao;
 
@@ -64,14 +52,13 @@ void menu(int opcao)
             cout << "Digite o ano de fundação: ";
             cin >> anoFundacao;
 
-            Clube* novoClube = new Clube(nome, anoFundacao);
+            Clube *novoClube = new Clube(nome, anoFundacao);
             cout << "Novo clube criado: " << novoClube->getNome() << endl;
             clubes.push_back(novoClube);
             break;
         }
 
-    case 2:
-        {
+        case 2: {
             string nome;
             int idade;
             string posicao;
@@ -91,8 +78,7 @@ void menu(int opcao)
             // Limpar o buffer após ler um int e antes de ler string (getline)
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-            do
-            {
+            do {
                 cout << "Digite a posicao do atleta: ";
                 getline(cin, posicao);
                 posicoes.push_back(posicao);
@@ -103,35 +89,29 @@ void menu(int opcao)
 
                 // Limpar o buffer novamente para o próximo loop do getline funcionar
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-            while (opcao == 1);
+            } while (opcao == 1);
 
-            Atleta* novoAtleta = new Atleta(nome, idade, posicoes);
+            Atleta *novoAtleta = new Atleta(nome, idade, posicoes);
             cout << "Novo atleta criado: " << novoAtleta->getNome() << endl;
 
             cout << "Deseja inserir o atleta à um clube?(1- Sim/ 2- Não)" << endl;
             cin >> opcao;
-            if (opcao == 1)
-            {
+            if (opcao == 1) {
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 cout << "Digite o nome do Clube: " << endl;
                 getline(cin, nome);
 
-                Clube* c = buscarClube(nome);
-                if (c != nullptr)
-                {
+                Clube *c = buscarClube(nome);
+                if (c != nullptr) {
                     c->comprarAtleta(novoAtleta);
-                }
-                else
-                {
+                } else {
                     cout << "Clube não encontrado!" << endl;
                 }
             }
         }
         break;
 
-    case 3:
-        {
+        case 3: {
             string nome;
             cout << "====Exibir Clube====\n";
 
@@ -141,22 +121,18 @@ void menu(int opcao)
             cout << "Digite o nome do Clube: " << endl;
             getline(cin, nome);
 
-            Clube* c = buscarClube(nome);
-            if (c != nullptr)
-            {
+            Clube *c = buscarClube(nome);
+            if (c != nullptr) {
                 cout << "Exibindo o clube: " << c->getNome() << endl;
                 c->exibirTitulos();
                 c->exibirElenco();
-            }
-            else
-            {
+            } else {
                 cout << "Clube não encontrado!" << endl;
             }
             break;
         }
 
-    case 4:
-        {
+        case 4: {
             string nome;
             cout << "====Exibir jogador====\n";
 
@@ -166,7 +142,7 @@ void menu(int opcao)
             cout << "Digite o nome do jogador: ";
             getline(cin, nome);
 
-            Atleta* a = buscarAtleta(nome);
+            Atleta *a = buscarAtleta(nome);
             if (a != nullptr)
                 a->exibir();
             else
@@ -175,8 +151,7 @@ void menu(int opcao)
             break;
         }
 
-    case 5:
-        {
+        case 5: {
             string nome;
             string novoClube;
             cout << "====Transferência de Jogador====\n";
@@ -184,44 +159,44 @@ void menu(int opcao)
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             getline(cin, nome);
 
-            Atleta* a = buscarAtleta(nome);
-            if (a != nullptr)
-            {
+            Atleta *a = buscarAtleta(nome);
+            if (a != nullptr) {
                 cout << "Digite o novo clube do jogador: " << endl;
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 getline(cin, novoClube);
-                Clube* c = buscarClube(novoClube);
-                a->transferencia(buscarClube(c->getNome()));
-            }
-            else
-            {
+                Clube *c = buscarClube(novoClube);
+                if (c != nullptr) {
+                    a->transferencia(c);
+                    cout << "Transferência realizada com sucesso!" << endl;
+                } else {
+                    cout << "Novo clube não encontrado!" << endl;
+                }
+            } else {
                 cout << "Atleta não encontrado!" << endl;
             }
             break;
         }
 
-    case 6:
-        cout << superMundial.getNome() << "\n";
-        cout << "\nCampeão: " << superMundial.getCampeao() << "\n" << endl;
-        superMundial.exibirClubes();
-        break;
+        case 6:
+            cout << superMundial.getNome() << "\n";
+            cout << "\nCampeão: " << superMundial.getCampeao() << "\n"
+                    << endl;
+            superMundial.exibirClubes();
+            break;
 
-    case 0:
-        cout << "Saindo...\n";
-        break;
+        case 0:
+            cout << "Saindo...\n";
+            break;
 
-    default:
-        cout << "Opcao inválida! Tente novamente.\n";
+        default:
+            cout << "Opcao inválida! Tente novamente.\n";
     }
 }
 
-int main()
-{
+int main() {
     adicionarCampeao();
     int opcao;
 
-    do
-    {
+    do {
         cout << "\n===== MENU PRINCIPAL =====\n";
         cout << "1. Cadastrar clube\n";
         cout << "2. Cadastrar jogador\n";
@@ -234,8 +209,7 @@ int main()
         cin >> opcao;
 
         menu(opcao);
-    }
-    while (opcao > 0);
+    } while (opcao > 0);
 
     return 0;
 }
